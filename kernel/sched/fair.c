32f8602e12cff6b6aefbecae08c421ed364b7bdd
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-01-29 10:14:16 +0530

    sched/fair: Fix schedtune boosted tasks placement issues
    The current code starts the CPU search with max capacity cluster
    for schedtune boosted tasks. If any of the CPU in this cluster
    can accommodate the task without exceeding its original capacity,
    the search is not extended to other clusters. This approach results
    in sub-optimal performance on tri-cluster systems with 1 CPU in the
    max capacity cluster. The max capacity CPU is packed with several
    tasks while the other mid capacity CPUs are idle. Fix this issue by
    starting the CPU search with mid capacity cluster as long as the
    task fits. The search is also expanded to the max capacity cluster
    and the best CPU is selected among the iterated CPUs. The best CPU
    is either an idle CPU in the shallowest c-state or CPU with maximum
    spare capacity when all the iterated CPUs are busy. The energy
    evaluation is also skipped for boosted tasks to maximize the
    performance.
    
    While at it, extend the sched_task_util trace point to print if
    the task is schedtune boosted or not.
    
    Change-Id: Iefb29e9d32f1c414bd6af31d66306f04c6f57f9c
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [Fixup merge errors - No mid cpus for SD845]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7487,7 +7487,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		 * next cluster if they are higher in capacity. If we are
 		 * not in any kind of boost, we break.
 		 */
-		if (!prefer_idle &&
+		if (!prefer_idle && !boosted &&
 			(target_cpu != -1 || best_idle_cpu != -1) &&
 			(fbt_env->placement_boost == SCHED_BOOST_NONE ||
 			sched_boost() != FULL_THROTTLE_BOOST ||
@@ -7498,9 +7498,12 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		/*
 		 * if we are in prefer_idle and have found an idle cpu,
 		 * break from searching more groups based on the stune.boost and
-		 * group cpu capacity.
+		 * group cpu capacity. For !prefer_idle && boosted case, don't
+		 * iterate lower capacity CPUs unless the task can't be
+		 * accommodated in the higher capacity CPUs.
 		 */
-		if (prefer_idle && best_idle_cpu != -1) {
+		if ((prefer_idle && best_idle_cpu != -1) ||
+		    (boosted && (best_idle_cpu != -1 || target_cpu != -1))) {
 			if (boosted) {
 				if (!next_group_higher_cap)
 					break;
